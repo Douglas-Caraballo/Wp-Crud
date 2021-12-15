ff9ba9629363b45e65acf1f6d3acde0397f1bb26
Doug <karaballo.douglas@hotmail.com>
2021-12-15 13:51:38 -0400

    Modificaciones a las paginas del plugin del lado del administrador

--- admin/option_menu.php
+++ admin/option_menu.php
@@ -139,6 +139,9 @@ function SubmenuReporte(){
         <h2><?php _e('Generar Reportes', 'generar reporte') ?></h2>
         <div class="Wrapper">
             <a class="Wp-CrudSubmit Button" href="<?= admin_url().'admin.php?page=reportes_general'?>">Generar</a>
+            <figure class="AdminFormContentImg">
+                <img class="AdminImg" src="<?= WPC_RUTA_PUBLIC.'admin/img/reportes.svg'; ?>" alt="">
+            </figure>
         </div>
     </div>
 <?php
