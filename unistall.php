45e791b6ea94841a8243b8ecc36aeb30e27f02cb
Doug <karaballo.douglas@hotmail.com>
2021-12-22 10:33:52 -0400

    Modificando el unistall para comprovar que elimine los datos

--- unistall.php
+++ unistall.php
@@ -9,30 +9,17 @@ global $wpdb;
 $nombreTabla = $wpdb->prefix."registers";
 $nombreTablaSecond= $wpdb->prefix."categories_registers";
 
-$sqlRegister="DROP TABLE IF EXISTS $nombreTabla";
+$sqlRegister="DROP TABLE IF EXISTS {$nombreTabla}";
 $wpdb -> query($sqlRegister);
 
-$sqlCategoriesRegister = "DROP TABLE IF EXISTS $nombreTablaSecond";
+$sqlCategoriesRegister = "DROP TABLE IF EXISTS {$nombreTablaSecond}";
 $wpdb -> query($sqlCategoriesRegister);
 
 /**
  * Elimina las paginas creadas por el plugin
  */
 
-$pagina = get_pages( array(
-    'authors'=> 1,
-    'post_type'=>'page',
-    'post_status' => 'publish',
-    'sort_column' => 'post_title'
-    )
-);
+wp_trash_post( get_option('Registrar'));
+wp_trash_post( get_option('Todos los Registros'));
 
-foreach ($pagina as $paginas){
-    $nombre= $paginas -> post_title;
-    if($nombre=='Todos los Registros'){
-        wp_delete_post( $paginas-> ID , true );
-    }
-    if ($nombre=='Registrar'){
-        wp_delete_post( $paginas-> ID , true );
-    }
-}
+wp_cache_flush();
