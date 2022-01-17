6b051686408e02d2c9faed66b7f7dcb358a6fc39
Doug <karaballo.douglas@hotmail.com>
2022-01-17 08:30:14 -0400

    Modificaciones a los estilos generales en la tabla de todos los registros

--- admin/vistas/submenu_todos.php
+++ admin/vistas/submenu_todos.php
@@ -5,9 +5,9 @@ function SubMenuTodos(){
         <h2><?php _e('Todos los Registros', 'todos los registros') ?></h2>
         <div class="Table">
             <div class="TableItem">
-                <article class="TableItemRegister">Nombre</article>
-                <article class="TableItemRegister">Fecha</article>
-                <article class="TableItemRegister">Categoria</article>
+                <article class="TableItemRegister TableRegisterHead">Nombre</article>
+                <article class="TableItemRegister TableRegisterHead">Fecha</article>
+                <article class="TableItemRegister TableRegisterHead">Categoria</article>
                 <article class="TableItemRegister"></article>
                 <article class="TableItemRegister"></article>
             </div>
